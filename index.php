8e5f65b24c6d5d1cb014b08efe2c490ffc44c8d5
Raji-Abdo <abdoraji2002@gmail.com>
2022-04-26 19:43:56 +0000

    nav

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php include 'src/extras/header.php' ?>
 
-<header>
-    <nav>
-      <ul>
-          <li class="logo"><a href="">Gestion des Emprutns</a></li>
-          <li class="login"><a href="">Login</a></li>
-          <li class="lib"><a href="">Library</a></li>
-      </ul>
-    </nav>
-  </header>
+
 
 
 
--- src/extras/header.php
+++ src/extras/header.php
@@ -7,7 +7,19 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="src/styles/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title>Library</title>
 </head>
 <body>
+  <header>
+    <nav>
+      <ul>
+          <li class="logo"><a href="">Gestion des Emprutns</a></li>
+          <li class="search_li"><input type="text" name="search" class="search" id=""></input><i class="fas fa-search"></i></li>
+          <li class="login"><a href="">Login</a></li>
+          <li class="lib"><a href="">Library</a></li>
+      </ul>
+    </nav>
+  </header>
+  
   
